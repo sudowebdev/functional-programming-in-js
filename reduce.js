0d82c8418da7367e3b78c6bfe9ede15358544ee7
Nikhil Arora <narora200@gmail.com>
2017-07-17 23:22:07 +0530

    reduce function implementation

--- reduce.js
+++ reduce.js
@@ -1,3 +1,16 @@
 /**
  * Created by iamoperand on 17/7/17.
  */
+var orders = [
+    {name: 'Momos', amount: 50},
+    {name: 'Pizza', amount: 250},
+    {name: 'Chowmein', amount: 70},
+    {name: 'Samosa', amount: 10},
+    {name: 'Patty', amount: 12}
+];
+
+var totalBillAmount = orders.reduce(function(sum, order){
+    return sum + order.amount;
+}, 0);
+
+console.log("The total bill amount is: ", totalBillAmount);

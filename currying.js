18c3b739a18cd4c1e90e45627f5c64890e7a4bef
Nikhil Arora <narora200@gmail.com>
2017-07-18 12:34:00 +0530

    Curried function implementation (Part 2)

--- currying.js
+++ currying.js
@@ -10,3 +10,17 @@ function notACurriedFunction(parameter1, parameter2, parameter3, parameter4){
 }
 
 console.log(notACurriedFunction("Not", "A", "Curried", "Function"));
+
+
+function aCurriedFunction(parameter1){
+    "use strict";
+    return function (parameter2){
+        return function(parameter3){
+
+                return parameter1 + " " + parameter2 + " " + parameter3 ;
+
+        }
+    }
+}
+
+console.log(aCurriedFunction("A")("Curried")("Function"));
